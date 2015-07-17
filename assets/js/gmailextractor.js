b412212ed0a8d383196e27bb02d63b12c17b5a1a
Bradley <golden.bradley@gmail.com>
2015-07-17 16:10:40 +0000

    added modal prompt if images arent selected

--- assets/js/gmailextractor.js
+++ assets/js/gmailextractor.js
@@ -1,301 +1,310 @@
-
 jQuery(function ($) {
 
-  var prog_hidden = true,
-    results_hidden = true, //added
-    loc = window.location,
-    $prog_container = $(".progress"),
-    $prog = $(".progress-bar"),
-    $results_container = $(".results"), //added
-    $email = $("#email"),
-    $pass = $("#password"),
-    $submit = $("#submit"),
-    $auth_form = $("#auth-form"),
-    $auth_fields = $auth_form.find(":input"),
-    $alert = $(".alert"),
-    $sync_form = $("#sync-form"),
-    $confim_form = $("#confirm-form"),
-    $no_confirm_bttn = $confim_form.find("[type=cancel]"),
-    $delete = $("#delete"),
-    $select_all = $("#select-all"),
-    select_bool = false,
-    $image_menu = $("#image-menu"),
-    rewrite_index = 0,
-    rewrite_total = 0,
-    feedback,
-  num_messages,
-  update_progress,
-  hide_progress,
-  update_results, //added 
-  img_id = 0,
-    hide_results, //added
-  selected_imgs = [],
-    ws = new WebSocket("ws://" + loc.host + "/ws");
-
-  //added
-  hide_results = function () {
-
-    $results_container.fadeOut();
-    results_hidden = true;
-  };
-
-  //displays images in the browser as they are found in the users mailbox
-  update_results = function (msg_id, img_id, enc_img, signed_req) {
+	var prog_hidden = true,
+		results_hidden = true, //added
+		loc = window.location,
+		$prog_container = $(".progress"),
+		$prog = $(".progress-bar"),
+		$results_container = $(".results"), //added
+		$email = $("#email"),
+		$pass = $("#password"),
+		$submit = $("#submit"),
+		$auth_form = $("#auth-form"),
+		$auth_fields = $auth_form.find(":input"),
+		$alert = $(".alert"),
+		$sync_form = $("#sync-form"),
+		$confim_form = $("#confirm-form"),
+		$no_confirm_bttn = $confim_form.find("[type=cancel]"),
+		$delete = $("#delete"),
+		$select_all = $("#select-all"),
+		select_bool = false,
+		$image_menu = $("#image-menu"),
+		$save = $("#save"),
+		rewrite_index = null,
+		rewrite_total = null,
+		feedback = null,
+		num_messages = null,
+		update_progress = null,
+		hide_progress = null,
+		update_results = null, //added 
+		img_id = null,
+		hide_results = null, //added
+		selected_imgs = [],
+		ws = new WebSocket("ws://" + loc.host + "/ws");
+
+	//added
+	hide_results = function () {
+
+		$results_container.fadeOut();
+		results_hidden = true;
+	};
+
+	//displays images in the browser as they are found in the users mailbox
+	update_results = function (msg_id, img_id, enc_img, signed_req) {
+
+		if (results_hidden) {
+
+			$results_container.show();
+			results_hidden = false;
+		}
 
-    if (results_hidden) {
+		//decode image from base64 to small image to display in img tag
+		var img = new Image();
+		img.src = 'data:image/jpeg;base64,' + enc_img;
+
+		//create thumbnail for image to be displayed in
+		//create a unique img_id for the purpose of selecting each image
+		$results_container.append('<div class="col-xs-6 col-md-3">' + 
+								  '<div class="thumbnail">' +
+								  '<input class="img-checkbox" id="' + img_id + 
+								  '" name="' + signed_req + '" type="checkbox">' +
+								  '<img src="' + img.src + '">' +
+								  '</div>' + 
+								  '</div>');
+	};
+	hide_progress = function () {
+		$prog_container.fadeOut();
+		prog_hidden = true;
+	};
+
+	update_progress = function (cur, max) {
+
+		if (prog_hidden) {
+			$prog_container.fadeIn();
+			prog_hidden = false;
+		}
+
+		if (!cur && !max) {
+
+			$prog_container.addClass("progress-striped").addClass("active");
+			$prog.attr("aria-valuenow", 1)
+			.attr("aria-valuemax", 1)
+			.css("width", "100%");
+
+		} else {
+
+			$prog_container.removeClass("progress-striped").removeClass("active");
+			$prog.attr("aria-valuenow", cur)
+			.attr("aria-valuemax", max)
+			.css("width", ((cur / max) * 100) + "%");
+		}
+	};
+
+	feedback = function (msg, additional_message) {
+
+		$alert.removeClass("alert-info").removeClass("alert-warning");
+		$alert.show();
+
+		if (msg.ok) {
 
-      $results_container.show();
-      results_hidden = false;
-    }
+			$alert.addClass("alert-info");
 
-    //decode image from base64 to small image to display in img tag
-    var img = new Image();
-    img.src = 'data:image/jpeg;base64,' + enc_img;
+		} else {
 
-    //create thumbnail for image to be displayed in
-    //create a unique img_id for the purpose of selecting each image
-    $results_container.append('<div class="col-xs-6 col-md-3">' + 
-                              '<div class="thumbnail">' +
-                              '<input class="img-checkbox" id="' + img_id + '" name="' + signed_req + '" type="checkbox">' +
-                              '<img src="' + img.src + '">' +
-                              '</div>' + 
-                              '</div>');
-  };
-  hide_progress = function () {
-    $prog_container.fadeOut();
-    prog_hidden = true;
-  };
+			$alert.addClass("alert-warning");
 
-  update_progress = function (cur, max) {
-
-    if (prog_hidden) {
-      $prog_container.fadeIn();
-      prog_hidden = false;
-    }
+		}
 
-    if (!cur && !max) {
+		if (additional_message) {
 
-      $prog_container.addClass("progress-striped").addClass("active");
-      $prog.attr("aria-valuenow", 1)
-      .attr("aria-valuemax", 1)
-      .css("width", "100%");
+			$alert.html("<p>" + msg.msg + "</p><p>" + additional_message + "</p>");
 
-    } else {
+		} else {
 
-      $prog_container.removeClass("progress-striped").removeClass("active");
-      $prog.attr("aria-valuenow", cur)
-      .attr("aria-valuemax", max)
-      .css("width", ((cur / max) * 100) + "%");
-    }
-  };
+			$alert.text(msg.msg);
 
-  feedback = function (msg, additional_message) {
+		}
+	};
 
-    $alert.removeClass("alert-info").removeClass("alert-warning");
-    $alert.show();
+	$select_all.click(function(){
 
-    if (msg.ok) {
+		var img_id = [];
 
-      $alert.addClass("alert-info");
+		//select all inputs if not selected
+		if(select_bool === false){
 
-    } else {
+			$("input.img-checkbox").prop("checked", true);	
 
-      $alert.addClass("alert-warning");
+			$("input.img-checkbox").each(function(){
+				img_id.push([$(this).attr("name"), $(this).attr("id")]); 
+			}); 
 
-    }
+			select_bool = true;
 
-    if (additional_message) {
+			//push all selected images to selected images array
+			selected_imgs.push(img_id);
 
-      $alert.html("<p>" + msg.msg + "</p><p>" + additional_message + "</p>");
+			//change name of button to deselect all
+			$("#select-all").text("Deselect All");
+		}
 
-    } else {
+		//deselect all inputs if selected
+		else {
 
-      $alert.text(msg.msg);
+			$("input").prop("checked", false);	
+			select_bool = false;
 
-    }
-  };
+			//pop all selected images in selected images array
+			selected_imgs = [];
 
-  $select_all.click(function(){
+			//change name of button to select all
+			$("#select-all").text("Select All");
+		}
+	});
 
-    var img_id = [];
+	//on click sends selected images to server to retreive full sized images
+	$save.click(function(){
+		//if not images are selected, display modal
+		if (selected_imgs.length === 0) {
+			$('#saveModal').modal('show');
+		}
+	});
 
-    //select all inputs if not selected
-    if(select_bool === false){
+	$auth_form.submit(function () {
 
-      $("input.img-checkbox").prop("checked", true);  
+		var params = JSON.stringify({
+			"email": $email.val(),
+			"pass": $pass.val(),
+			"type": "connect",
+			"limit": 0,
+			"simultaneous": 10,
+			"rewrite": 1
+		});
 
-      $("input.img-checkbox").each(function(){
-        img_id.push([$(this).attr("name"), $(this).attr("id")]); 
-      }); 
+		$auth_fields.attr("disabled", "disabled");
+		ws.send(params);
 
-      select_bool = true;
+		return false;
+	});
 
-      //push all selected images to selected images array
-      selected_imgs.push(img_id);
+	$sync_form.submit(function () {
 
-      //change name of button to deselect all
-      $("#select-all").text("Deselect All");
-    }
+		var params = JSON.stringify({
+			"type": "sync"
+		});
 
-    //deselect all inputs if selected
-    else {
+		$(this).find("[type=submit]").attr("disabled", "disabled");
+		ws.send(params);
 
-      $("input").prop("checked", false);  
-      select_bool = false;
+		return false;
+	});
 
-      //pop all selected images in selected images array
-      selected_imgs = [];
+	//TODO - sync selected images for deletion with web server
+	$delete.click(function () {
 
-      //change name of button to select all
-      $("#select-all").text("Select All");
-    }
-  });
+		var params = JSON.stringify({
+			"type": "delete",
+		});
 
-  $auth_form.submit(function () {
+		ws.send(params);
 
-    var params = JSON.stringify({
-      "email": $email.val(),
-      "pass": $pass.val(),
-      "type": "connect",
-      "limit": 0,
-      "simultaneous": 10,
-      "rewrite": 1
-    });
+	});
 
-    $auth_fields.attr("disabled", "disabled");
-    ws.send(params);
+	$confim_form.submit(function () {
 
-    return false;
-  });
+		var params = JSON.stringify({
+			"type": "confirm",
+		});
 
-     $sync_form.submit(function () {
+		$(this).find("button").attr("disabled", "disabled");
+		ws.send(params);
 
-     var params = JSON.stringify({
-     "type": "sync"
-     });
+		return false;
+	});
 
-     $(this).find("[type=submit]").attr("disabled", "disabled");
-     ws.send(params);
+	$no_confirm_bttn.click(function () {
 
-     return false;
-     });
+		feedback({msg: "Thank you for your participation in this study."});
+		$confim_form.fadeOut();
+		return false;
+	});
 
-  //TODO - sync selected images for deletion with web server
-  $delete.click(function () {
+	/* Adds the signed hmac key to an array
+	 * that is sent back to the server for deletion   
+	 */
+	$(document).on( "click", "input.img-checkbox", function() {
 
-    var params = JSON.stringify({
-      "type": "delete",
-    });
+		var img_id = [ $(this).attr("name"), $(this).attr("id") ];
+		var is_checked = $(this).prop('checked');
 
-    ws.send(params);
+		//checkbox is clicked, save filename in an array
+		if(is_checked){
 
-  });
+			selected_imgs.push(img_id);
+		}
+		//checkbox is unclicked, remove filename from the array
+		else {
 
-  $confim_form.submit(function () {
+			var index = selected_imgs.indexOf(img_id); 
+			selected_imgs.splice(index, 1);
+		}
+	});
 
-    var params = JSON.stringify({
-      "type": "confirm",
-    });
+	ws.onmessage = function (evt) {
+		var msg = JSON.parse(evt.data);
 
-    $(this).find("button").attr("disabled", "disabled");
-    ws.send(params);
+		switch (msg['type']) {
 
-    return false;
-  });
+			case "connect":
+				feedback(msg);
+			if (!msg.ok) {
+				$auth_fields.removeAttr("disabled");
+			} else {
+				$auth_form.fadeOut();
+			}
+			break;
 
-  $no_confirm_bttn.click(function () {
+			case "count":
+				feedback(msg);
+			num_messages = msg.num;
+			break;
 
-    feedback({msg: "Thank you for your participation in this study."});
-    $confim_form.fadeOut();
-    return false;
-  });
+			case "image": //added
+				$image_menu.fadeIn();
+			update_results(msg.msg_id, msg.img_id, msg.enc_img, msg.hmac_key);
 
-  /* Adds the signed hmac key to an array
-   * that is sent back to the server for deletion   
-   */
-  $(document).on( "click", "input.img-checkbox", function() {
+			case "downloading":
+				feedback(msg);
+			update_progress(msg.num, num_messages);
+			break;
 
-    var img_id = [ $(this).attr("name"), $(this).attr("id") ];
-    var is_checked = $(this).prop('checked');
+			case "download-complete":
+				feedback(msg, "Please check all attachments you'd like removed from your GMail account");
+			hide_progress();
+			//$sync_form.fadeIn();
+			break;
 
-    //checkbox is clicked, save filename in an array
-    if(is_checked){
+			case "file-checking":
+				feedback(msg);
+			update_progress();
+			//$sync_form.fadeOut();
+			break;
 
-      selected_imgs.push(img_id);
-    }
-    //checkbox is unclicked, remove filename from the array
-    else {
+			case "file-checked":
+				rewrite_total = msg.num;
+			hide_progress();
+			$alert.hide();
+			$confim_form
+			.fadeIn()
+			.find("p")
+			.text("Are you sure you want to remove " + rewrite_total + " images from your email account?  This action is irreversable.");
+			break;
 
-      var index = selected_imgs.indexOf(img_id); 
-      selected_imgs.splice(index, 1);
-    }
-  });
+			case "removing":
+				$confim_form.fadeOut();
+			feedback(msg);
+			update_progress(++rewrite_index, rewrite_total);
+			break;
 
-  ws.onmessage = function (evt) {
-    var msg = JSON.parse(evt.data);
+			case "removed":
+				feedback(msg);
+			break;
 
-    switch (msg['type']) {
-
-      case "connect":
-        feedback(msg);
-      if (!msg.ok) {
-        $auth_fields.removeAttr("disabled");
-      } else {
-        $auth_form.fadeOut();
-      }
-      break;
-
-      case "count":
-        feedback(msg);
-      num_messages = msg.num;
-      break;
-
-      case "image": //added
-        $image_menu.fadeIn();
-      update_results(msg.msg_id, msg.img_id, msg.enc_img, msg.hmac_key);
-
-      case "downloading":
-        feedback(msg);
-      update_progress(msg.num, num_messages);
-      break;
-
-      case "download-complete":
-        feedback(msg, "Please check all attachments you'd like removed from your GMail account");
-      hide_progress();
-      //$sync_form.fadeIn();
-      break;
-
-      case "file-checking":
-        feedback(msg);
-      update_progress();
-      //$sync_form.fadeOut();
-      break;
-
-      case "file-checked":
-        rewrite_total = msg.num;
-      hide_progress();
-      $alert.hide();
-      $confim_form
-      .fadeIn()
-      .find("p")
-      .text("Are you sure you want to remove " + rewrite_total + " images from your email account?  This action is irreversable.");
-      break;
-
-      case "removing":
-        $confim_form.fadeOut();
-      feedback(msg);
-      update_progress(++rewrite_index, rewrite_total);
-      break;
-
-      case "removed":
-        feedback(msg);
-      break;
-
-      case "finished":
-        feedback(msg);
-      hide_progress();
-      break;
-    }
-  };
+			case "finished":
+				feedback(msg);
+			hide_progress();
+			break;
+		}
+	};
 
 });
